3b7a039a65cfe16768a53b3f5acd2a7bd0a0bce8
Priscila Moreno <priscila.moreno.wow@gmail.com>
2017-11-17 19:01:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName(string){
+  var customerName = "bob";
+  return customerName.toUpperCase();
+}
+upperCaseCustomerName();
+
+function setBestCustomer(string){
+  var bestCustomer = "not bob";
+  return bestCustomer;
+}
+
+setBestCustomer();
+
+function overwriteBestCustomer(string){
+  var bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = "bob";
+
+function changeLeastFavoriteCustomer(string){
+  const leastFavoriteCustomer = "joe";
+  return leastFavoriteCustomer;
+}
+changeLeastFavoriteCustomer();
+
+
+function attemptTwoFavoriteCustomers(string){
+  let bestCustomer = "not bob";
+  let bestCustomer = "joe";
+  return bestCustomer;
+}
+attemptTwoFavoriteCustomers();
