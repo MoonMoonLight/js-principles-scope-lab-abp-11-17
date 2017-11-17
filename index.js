7a7008763a5753e51372e047855e60c2dbb3ec15
Priscila Moreno <priscila.moreno.wow@gmail.com>
2017-11-17 19:30:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var customerName = "bob";
 
-function upperCaseCustomerName(string){
+/*function upperCaseCustomerName(string){
   var customerName = "bob";
   return customerName.toUpperCase();
 }
